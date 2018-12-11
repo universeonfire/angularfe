8ad811224413b26aa63dbe12d19fdfd84d4028dc
Evren <evren@Evren-iMac.local>
2018-12-11 13:13:07 +0300

    Servis ayarlama başladı

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { HomepageComponent } from './homepage/homepage.component';
 import { DocumentsComponent } from './documents/documents.component';
+import { DocumentService } from './documents/document.service';
 import { ProposalListComponent } from './proposal/proposal-list.component';
 import { ProposalNewContent } from './proposal/proposal-new.component';
 import { ProposalShowComponent } from './proposal/proposal-show.component';
@@ -27,7 +28,9 @@ import { ProposalShowComponent } from './proposal/proposal-show.component';
     NgbModule.forRoot(),
     FormsModule,
   ],
-  providers: [],
+  providers: [
+    DocumentService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/documents/document.service.ts
@@ -0,0 +1,10 @@
+import  { Injectable } from "@angular/core";
+import  { Http, Response, Headers, RequestOptions } from "@angular/http";
+
+
+
+@Injectable()
+
+export class DocumentService{
+
+}
